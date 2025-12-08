a2d138d0f7ad3b292aab96466281300efad0476d
TakiyaYoru <duykhafoot@gmail.com>
2025-12-08 08:42:29 +0700

    feat: Thêm license

--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2025 Green-AQI Navigator Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 'use client';
 import { useEffect, useState } from 'react';
 import { 
@@ -6,6 +23,7 @@ import {
   BarChart, Bar, ComposedChart, Area
 } from 'recharts';
 import { AnalyticsData, fetchAnalytics } from './analyticsService';
+import { Licorice } from 'next/font/google';
 
 // Màu sắc cho biểu đồ tròn
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884d8'];
